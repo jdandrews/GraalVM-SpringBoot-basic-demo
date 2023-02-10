3af99a115ef6996c93f2c77e3bc8d99771d2e76a
Jerry Andrews <jerry@jrandrews.org>
2023-02-10 13:26:43 -0800

    support for the time-wasting endpoint

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,39 +9,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
-	}
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/iterate")
+    public static String countUppers(@RequestParam(value = "strings",
+            defaultValue = "Now is the Time for All good Men to Come to the aid of their party!") String sentence,
+            @RequestParam(value="repeats", defaultValue = "10_000_000") int repeats) {
+        int iterations = 1;
+        int outerLoopRepeats = Integer.valueOf(repeats);
+        StringBuilder result = new StringBuilder("iterations:\n");
+        
+        for (int iter = 0; iter < iterations; iter++) {
+            if (iterations != 1) {
+                result.append(String.format("-- iteration " + (iter + 1) + " --%n"));
+            }   
+            long total = 0, start = System.currentTimeMillis(), last = start;
+            for (int i = 1; i < outerLoopRepeats; i++) {
+                total += sentence
+                  .chars()
+                  .filter(Character::isUpperCase)
+                  .count();
+                if (i % 1_000_000 == 0) {
+                    long now = System.currentTimeMillis();
+                    result.append(String.format("\t%d (%d ms)%n", i / 1_000_000, now - last));
+                    last = now;
+                }   
+            }   
+            result.append(String.format("total: %d (%d ms)%n", total, System.currentTimeMillis() - start));
+        }   
+        
+        return result.toString();
+    }
 }
-
-	// @GetMapping("/iterate")
-	// 	public static String countUppers(@RequestParam(value = "strings",
-	// 			defaultValue = "Now is the Time for All good Men to Come to the aid of their party!") String sentence) {
-	// 		int ITERATIONS = 1;
-	// 		StringBuilder result = new StringBuilder("iterations:\n");
-			
-	// 		for (int iter = 0; iter < ITERATIONS; iter++) {
-	// 			if (ITERATIONS != 1) {
-	// 				result.append(String.format("-- iteration " + (iter + 1) + " --%n"));
-	// 			}   
-	// 			long total = 0, start = System.currentTimeMillis(), last = start;
-	// 			for (int i = 1; i < 10_000_000; i++) {
-	// 				total += sentence
-	// 				.chars()
-	// 				.filter(Character::isUpperCase)
-	// 				.count();
-	// 				if (i % 1_000_000 == 0) {
-	// 					long now = System.currentTimeMillis();
-	// 					result.append(String.format("\t%d (%d ms)%n", i / 1_000_000, now - last));
-	// 					last = now;
-	// 				}   
-	// 			}   
-	// 			result.append(String.format("total: %d (%d ms)%n", total, System.currentTimeMillis() - start));
-	// 		}   
-			
-	// 		return result.toString();
-	// 	}
--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -15,13 +15,14 @@
  */
 package com.example.restservice;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -31,22 +32,48 @@ import org.springframework.test.web.servlet.MockMvc;
 @AutoConfigureMockMvc
 public class GreetingControllerTests {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, World!"));
-	}
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
 
-	@Test
-	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
+    @Test
+    public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
-				.andDo(print()).andExpect(status().isOk())
-				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
-	}
+        this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
+    }
 
+    @Test
+    public void noParamCountUppers() throws Exception {
+        var result = this.mockMvc
+                .perform(get("/iterate")
+                        .param("repeats", "1000001"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertThat(result, containsString("iterations:"));
+        assertThat(result, containsString("total: 5000000"));   // 5 capital letters in the default test string
+    }
+
+    @Test
+    public void suppliedStringCountUppers() throws Exception {
+        var result = this.mockMvc
+                .perform(get("/iterate")
+                        .param("strings", "This String has 2 capital letters.")
+                        .param("repeats", "1000001"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn().getResponse().getContentAsString();
+
+        assertThat(result, containsString("iterations:"));
+        assertThat(result, containsString("total: 2000000"));
+    }
 }
